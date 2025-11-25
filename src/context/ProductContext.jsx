7fdb23aac77b5625e583c2d0650a46e6a3b680c2
David Mvang <makouangoudavid@gmail.com>
2025-11-25 13:54:28 +0100

    Product Context - Fixes #5

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, createContext, useContext} from "react";
+
+const ProductContext = createContext();
+
+export function useProducts() {
+    return useContext(ProductContext)
+};
+
+export function ProductProvider({children}) {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+            const fetchProducts = async() => {
+            try{
+                const res = await fetch('/api/products')
+                if(!res.ok) throw new Error('Failed to catch products')
+                const data = await res.json();
+                console.log(data);
+                setProducts(data);
+            } catch(error){
+                setError(error.message);
+            } finally{
+                setLoading(false);
+            }
+            }
+
+            fetchProducts();
+    },[])
+
+    return(
+        <ProductContext.Provider value={{products, loading, error}}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
