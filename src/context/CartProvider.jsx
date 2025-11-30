7d58e8d364218457e50241c14825290d832e0c99
David Mvang <makouangoudavid@gmail.com>
2025-11-30 23:34:42 +0100

    store cart items to local storage - Fixes #15

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartContext } from "./CartContext";
 
 export function CartProvider({children}) {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        const stored = localStorage.getItem('cart')
+
+        return stored ? JSON.parse(stored) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart])
 
     const addToCart = (Product) => {
         setCart((prev) => {
